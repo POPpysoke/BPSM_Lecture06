5891d6e10dd3991de4d100ed94c31939c80fde4f
POPpysoke <joelrodenburg@gmail.com>
2024-10-05 14:43:12 +0100

    overconfidence is a slow and insidious killer

---
+++ HSP100a-20m-.sh
@@ -0,0 +1,14 @@
+echo "HSP's with >20 mismatches and <100 alignment length"
+while read query_acc_ver subject_acc_ver identity alignment_length mismatches gap_opens q_start q_end s_start s_end evalue bit_score
+do
+ if [[ "${alignment_length}" -lt 100 ]]
+ then
+    
+    if [[ "${mismatches}" -gt 20 ]]
+    then
+       echo  "protein length of ${alignment_length} with ${mismatches} mismatches"
+    
+       
+    fi
+ fi
+done < blastoutput2.out 
