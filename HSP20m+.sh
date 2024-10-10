00f2abb1d84badc322bfc4eac57e690286d4a218
POPpysoke <joelrodenburg@gmail.com>
2024-10-10 21:12:31 +0100

    updating older files with more correct and simple codes

--- HSP20m+.sh
+++ HSP20m+.sh
@@ -4,7 +4,7 @@ IFS=$'\t'
 while read query_acc_ver subject_acc_ver identity alignment_length mismatches gap_opens q_start q_end s_start s_end evalue bit_score
 do
 # Evaluation step
-if test ${mismatches} -ge 20
+if [[ ${mismatches} -ge 20 ]]
   then
    echo -e " HSP mismatch rate of ${mismatches}\t${query_acc_ver}\t${subject_acc_ver} was found"
    else
--- SubAscnHSP.sh
+++ SubAscnHSP.sh
@@ -1,3 +1,4 @@
+/usr/bin/bash
 IFS=$'\t'
 while read  query_acc_ver subject_acc_ver identity alignment_length mismatches gap_opens q_start q_end s_start s_end evalue bit_score
   do 
